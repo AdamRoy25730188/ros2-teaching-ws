1ae49d8fad88882cd4c54247947d45ca4e1423b0
AdamRoy25730188 <102680762+AdamRoy25730188@users.noreply.github.com>
2024-04-16 00:06:14 +0100

    Add files via upload

---
+++ src/my_package/my_package/my_node.py
@@ -0,0 +1,9 @@
+def main():
+    print('Hi from my_package.')
+    twist linear: x: 0.0 y: 0.0 z: 0.0
+    angular: x: 0.0 y: 0.0 z: 0.5
+    
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source opt/ros/humble/setup.bash
+
+ros2 launch uol_tidybot tidybot.launch.py
+
+ros2 topic
+
+ros2 node
