4d1f78eb84af2752de5bf99da41c57aaa4fb4a3f
AdamRoy25730188 <102680762+AdamRoy25730188@users.noreply.github.com>
2024-04-18 02:13:51 +0100

    Update move_node.py

--- src/my_package/my_package/move_node.py
+++ src/my_package/my_package/move_node.py
@@ -1,24 +1,24 @@
 import rclpy
 from geometry_msgs.msg import Twist
 
-class MyPublisher:
+class Publisher:
 
     def __init__(self):
-        super().__init__('MyPublisher')
+        super().__init__('move_node')
         self.pub = rclpy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=1)
 
     def run(self):
         while not rclpy.is_shutdown():
             s = Twist()
             s.angular.z = 0.5
-            print('debug: %s' % s)
+            #print('debug: %s' % s)
             self.pub.publish(s)
             rclpy.sleep(.3)
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = MyPublisher()
+    node = Publisher()
     rclpy.spin(node)
     rclpy.shutdown()
 
