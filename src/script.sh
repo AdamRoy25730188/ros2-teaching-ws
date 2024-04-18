20e3227533181293dc506cfa4f159815f75925f3
Lincoln Computing Student <student@socstech.support>
2024-04-18 09:57:06 +0000

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/script.sh
+++ src/script.sh
@@ -8,6 +8,8 @@ ros2 launch uol_tidybot tidybot.launch.py
 
 ros2 pkg create --build-type ament_python --license Apache-2.0 --node-name m_node my_pk
 
+ource install/local_setup.bash
+
 ros2 topic
 
 ros2 node
