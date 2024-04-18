ec3548e3cf058f09e4bd75b5edf23037ea783a57
AdamRoy25730188 <102680762+AdamRoy25730188@users.noreply.github.com>
2024-04-18 06:06:08 +0100

    Update script.sh

--- src/script.sh
+++ src/script.sh
@@ -6,6 +6,8 @@ docker login -u lcas -p lincoln lcas.lincoln.ac.uk
 
 ros2 launch uol_tidybot tidybot.launch.py
 
+ros2 pkg create --build-type ament_python --license Apache-2.0 --node-name my_node my_package
+
 ros2 topic
 
 ros2 node
