4843de875a1e6d2b69cb06acf63e981f2dfe7eec
AdamRoy25730188 <102680762+AdamRoy25730188@users.noreply.github.com>
2024-07-31 05:54:29 +0100

    Update colour_chaser.py

--- src/my_pk/my_pk/colour_chaser.py
+++ src/my_pk/my_pk/colour_chaser.py
@@ -120,7 +120,7 @@ class ColourChaser(Node):
                     
                     print("Centroid of the biggest area: ({}, {})".format(cx, cy))
 
-                    if cy < 100:
+                    if cy < 200:
     
                         # Draw a circle centered at centroid coordinates
                         #cv2.circle(image, center_coordinates, radius, color, thickness) -1 px will fill the circle
