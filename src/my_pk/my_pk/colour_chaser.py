9268fc5eedf4fdd4bdd50c5627b81fb81685f90b
AdamRoy25730188 <102680762+AdamRoy25730188@users.noreply.github.com>
2024-07-31 04:08:03 +0100

    Update colour_chaser.py

--- src/my_pk/my_pk/colour_chaser.py
+++ src/my_pk/my_pk/colour_chaser.py
@@ -177,28 +177,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-                 '''
-                    following is unfinished attempt to usilise lidar
-                    '''
-                    '''
-                    start_index = max(0, -45)
-                    end_index = min(360, 45)
-
-                    xDistFromCentre = abs(400 - cx) / 400
-
-
-                    rayDepth = self.min_range(self.rays.ranges[start_index:end_index])
-
-                    if(rayDepth < cx):
-                        self.get_logger().warning("Wall is somehow in front of box!")
-                    if(rayDepth - cx < 0.2 + xDistFromCentre/8): #xDistFromCentre to account for distortion
-                        self.get_logger().info(f'Box at {cx}, {cy} against wall')
-                        continue
-                    self.get_logger().info(f'Box at {cx}, {cy} to be pushed')
-                    '''
